9fba1b979aa11f843deb3a5b4c611135440bb973
Yimikami <hi@yimikami.me>
2024-07-04 17:32:19 +0300

    chore: Update background colors and add login page components

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main
       className="flex h-full flex-col items-center justify-center
-        bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400
+        bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-400
         to-blue-800"
     >
       <div className="space-y-6 text-center">
