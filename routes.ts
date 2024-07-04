8a7692590a7a09060545fb9cf95c85ace259e2fa
Yimikami <hi@yimikami.me>
2024-07-05 00:42:54 +0300

    feat: Add login functionality and form validation

---
+++ routes.ts
@@ -0,0 +1,26 @@
+/**
+ * An array of routes that are public.
+ * These routes are accessible without authentication.
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes that are used for authentication.
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * The prefix for the API authentication routes.
+ * Routes that start with this prefix are used for authentication.
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after a user logs in.
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
